101d367e195ab72ab3056451f7af188e8bba7cf0
saiteja <77820388+saitejakanumuri@users.noreply.github.com>
2021-09-15 10:49:12 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup as bs
-
-t_user = input("input user handle: ")
-url= 'https://twitter.com/'+t_user
-r=requests.get(url)
-soup=bs(r.content,'html.parser')
-profile_photo=soup.find('img',{'alt':'Profile photo'})
-print(profile_photo)
